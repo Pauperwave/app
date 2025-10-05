7d893b523e5037461c7f58d7c3786fc4d7d66502
Emanuele Nardi <emanuelenardi.dev@gmail.com>
2025-10-05 14:00:49 +0200

    feat: add suppressSuspenseWarning plugin to suppress experimental feature warnings in development

---
+++ app/plugins/suppressSuspenseWarning.ts
@@ -0,0 +1,15 @@
+// https://github.com/nuxt/nuxt/discussions/25973#discussioncomment-12203169
+export default defineNuxtPlugin(() => {
+  if (import.meta.env.DEV) {
+    const originalInfo = console.info
+    console.info = (...args) => {
+      if (
+        typeof args[0] === 'string'
+        && args[0].includes('<Suspense> is an experimental feature')
+      ) {
+        return // terminate msg
+      }
+      originalInfo(...args)
+    }
+  }
+})
